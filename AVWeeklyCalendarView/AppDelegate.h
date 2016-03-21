bb8e5f44bb3d1b4ed6b98cc62173dec72ca66ecc
Cass <282365956@qq.com>
2016-03-21 12:32:42 +0800

    Initial Commit

---
+++ AVWeeklyCalendarView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVWeeklyCalendarView
+//
+//  Created by Cass on 16/3/21.
+//  Copyright © 2016年 Cass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVWeeklyCalendarView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVWeeklyCalendarView
+//
+//  Created by Cass on 16/3/21.
+//  Copyright © 2016年 Cass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
